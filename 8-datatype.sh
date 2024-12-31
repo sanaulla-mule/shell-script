5f01defbc7392469fc8de0c9e8ddad348c13625e
Your Name <you@example.com>
2024-12-31 19:56:34 +0530

    datatype

--- 8-datatype.sh
+++ 8-datatype.sh
@@ -3,8 +3,8 @@
 NUMBER1=$1
 NUMBER2=$2
 
-TIMESTAMP=$(Date)
-echo "script exected at:$TIMESTAMP"
+TIMESTAMP=$(date)
+echo "script exected at: $TIMESTAMP"
 SUM=$(($NUMBER1+$NUMBER2))
 
 echo " sum of $NUMBER1 and $NUMBER2 is: $SUM"
