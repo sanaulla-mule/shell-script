74adaccfde8e2de63e3cad5496fd4c95408a3fd9
Your Name <you@example.com>
2025-01-02 19:58:46 +0530

    mysqlinstall

--- mysqlinstall.sh
+++ mysqlinstall.sh
@@ -7,8 +7,31 @@ if [ $USERID -ne 0 ]
 then 
 
  echo " ERROR:: you must have access the sudo user"
- exit 1
+ exit 1 # other then 0
 
  fi
 
 dnf install mysql -y
+
+if [$? -ne 0]
+
+then 
+
+  echo "Installing mql... Failure"
+  exit 1
+else 
+
+  echo " Installing mql ...SUCCESS"
+
+fi
+
+dnf install git -y
+
+if [$? -ne 0]
+
+then 
+
+  echo "Installing git .. Failure"
+  exit 1
+else 
+  echo " Installing  git ...SUCCESS"
