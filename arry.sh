54bf43432b25aaba24b5a4dae0f35c35afe9834e
Your Name <you@example.com>
2025-01-02 18:45:25 +0530

    mysqlinstall

---
+++ arry.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MOVIES=("puashpa" "rrr" "ram")
+
+echo "first movie:${MOVIES[0]}"
+echo "first movie:${MOVIES[1]}"
+echo "first movie:${MOVIES[2]}"
+
+echo "All movie:${MOVIES[@]}"
---
+++ condition.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+NUMBER=$1
+
+if [ $NUMBER -gt 1000 ]
+ then 
+
+  echo " given number is gater than 1000"
+
+  else 
+
+  echo " given number is less than or equql to  1000"
+
+  fi
+   
